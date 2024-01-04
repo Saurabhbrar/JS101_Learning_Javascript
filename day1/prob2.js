ab1e7e398fe613f35ba568c17be2e42b5f842d69
Saurabh Brar <saurabh.barar@gmail.com>
2024-01-04 20:18:37 +0530

    day 1 code

---
+++ day1/prob2.js
@@ -0,0 +1,3 @@
+let name = "Saurabh Brar";
+let age = 28;;
+console.log("My Name Is",name,"And My Age Is",age);
---
+++ day2/prob1.js
@@ -0,0 +1,21 @@
+let name = "Saurabh Brar" ;
+let school = "Masai";
+let section= "cap06" ;
+let roll= 223 ;
+let math= 90 ;  
+let science= 95 ;
+let english= 99 ;
+let total = 300 ;
+let obtain= math + science + english ;
+let persentage = obtain/total*100;
+
+console.log("Name =",name)
+console.log("school =",school)
+console.log("section =",section)
+console.log("roll no =",roll)
+console.log("Math =",math)
+console.log("science =",science)
+console.log("english =", english)
+
+console.log("obtain =",obtain)
+console.log("persentage =",persentage)
